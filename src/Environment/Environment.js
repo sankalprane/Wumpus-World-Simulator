ae4b4db07a5b97d9ebde046a9d0229cde873b296
sankalprane <sankalprane1998@gmail.com>
2023-03-23 12:29:37 -0500

    pits and breeze added

--- src/Environment/Environment.js
+++ src/Environment/Environment.js
@@ -6,11 +6,13 @@ const dir_col = [0, 0, 1, -1];
 
 export default function Environment() {
 
-    const [state, setState] = useState([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]);
+    const [state, setState] = useState([[null, null, null, null], [null, null, null, null], [null, null, null, null],  [null, null, null, null]]);
 
     useEffect(() => {
         console.log('inside useEffect');
         generateWumpus();
+        generatePit();
+        generatePit();
       }, []);
 
     function isValid(i, j) {
@@ -39,6 +41,41 @@ export default function Environment() {
             const newState = [...oldState];
             newState[i][j] = "W";
             generateStench(i, j, newState);
+            return newState;
+        })
+    }
+
+    function generateBreeze(x, y, state) {
+        for (let i = 0; i < 4; i++) {
+            let newX = x + dir_row[i];
+            let newY = y + dir_col[i];
+            if (isValid(newX, newY))
+                if (state[newX][newY] == 'Stench') {
+                    state[newX][newY] = 'Stench \nBreeze'
+                } else if (state[newX][newY] == 'W') {
+                    state[newX][newY] = 'W \nBreeze'
+                } else {
+                    state[newX][newY] = 'Breeze'
+                }
+               
+        }
+    }
+
+    function generatePit() {
+        const iMin = 0, iMax = 3, jMin = 0, jMax = 3;
+        let i = 0, j = 0;
+        while (i === 0 && j === 0) {
+            i = Math.floor(Math.random() * (iMax - iMin + 1)) + iMin;
+            j = Math.floor(Math.random() * (jMax - jMin + 1)) + jMin;
+        }
+        setState((oldState) => {
+            const newState = [...oldState];
+            if (newState[i][j] == "W") {
+                newState[i][j] = "W P"
+            } else {
+                newState[i][j] = "P";
+            }
+            generateBreeze(i, j, newState);
             return newState;
         })
     }
